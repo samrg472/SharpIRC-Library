0afd2605493d298b8f5005022af6909a550e3de8
samrg472 <samrg472@gmail.com>
2013-06-29 11:27:05 -0400

    Generic safety on all methods and convert arraylist to list

--- src/Core/Event/EventBus.cs
+++ src/Core/Event/EventBus.cs
@@ -7,23 +7,23 @@ namespace SharpIRC.Core.Event {
 
     public class EventBus {
 
-        private ArrayList registry = new ArrayList();
+        private List<Delegate> registry = new List<Delegate>();
 
-        public void register<T>(Action<T> method) {
+        public void register<T>(Action<T> method) where T : Event {
             if (!registry.Contains(method))
                 registry.Add(method);
         }
 
-        public void unregister<T>(Action<T> method) {
+        public void unregister<T>(Action<T> method) where T : Event {
             if (registry.Contains(method))
                 registry.Remove(method);
         }
 
-        public void post<T>(Event e) {
+        public void post<T>(Event e) where T : Event {
             post<T>(e, false);
         }
 
-        public void post<T>(Event e, bool block) {
+        public void post<T>(Event e, bool block) where T : Event {
             ThreadStart start = () => {
                 for (int i = 0; i < registry.Count; i++) {
                     Action<T> m = registry[i] as Action<T>;
